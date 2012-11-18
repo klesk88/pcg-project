9949944b5280ae4175e369964ae0e3385c073ba7
ermac988@gmail.com <ermac988@gmail.com@af11f6f2-b44f-d57f-2de4-35ac3e98ffae>
2012-11-18 20:50:07 +0000

    Add plugin folder and unity script

---
+++ PCGProject/Assets/Data.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using XMLParserUnity;
+using System.Collections.Generic;
+
+public class Data : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        XMLParser parser = new XMLParser();
+        parser.xmlParser();
+        XMLParser.FileToCreate[] type_of_file = new XMLParser.FileToCreate[2];
+        type_of_file[0] = XMLParser.FileToCreate.Buildings;
+        type_of_file[1] = XMLParser.FileToCreate.Streets;
+        parser.deleteAllFiles();
+        parser.createFile(type_of_file);
+        List<List<float[]>> data = new List<List<float[]>>(); 
+        Debug.Log("before: " + data.Count);
+        data = parser.read("Building.txt");
+        Debug.Log("after: " + data.Count);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
