b7db833d6d0a78ddefeb77593527e0296ed7b8d8
vAcuMo@gmail.com <vAcuMo@gmail.com@af11f6f2-b44f-d57f-2de4-35ac3e98ffae>
2012-11-26 13:18:36 +0000

    Added node and pathfinder class, and modified the data class to read roads from Highway.txt and instantiate nodes with cubes for visualization and pathfinding

--- PCGProject4.0/Assets/Data.cs
+++ PCGProject4.0/Assets/Data.cs
@@ -36,22 +36,20 @@ public class Data : MonoBehaviour {
             building.transform.parent = this.gameObject.transform;
             int j = 0;
             int height = 0;
+            float minX = Mathf.Infinity, maxX = -Mathf.Infinity, minZ = Mathf.Infinity, maxZ = -Mathf.Infinity;
             foreach (double[] dArray in data[i]) {
                 if (j < data[i].Count - 1) { // Ignore last node, since it's equal to the first
                     vertices[j].x = (float)dArray[0] * scalingFactor;
                     vertices[j].z = (float)dArray[1] * scalingFactor;
+                    if (vertices[j].x < minX) minX = vertices[j].x;
+                    if (vertices[j].x > maxX) maxX = vertices[j].x;
+                    if (vertices[j].z < minZ) minZ = vertices[j].z;
+                    if (vertices[j].z > maxZ) maxZ = vertices[j].z;
                     //Debug.Log("x: " + vertices[j].x + "   z; " + vertices[j].z);
                     if (dArray[2] != 0)
                         height = (int)dArray[2];
                     j++;
                 }   
-            }
-            float minX = Mathf.Infinity, maxX = -Mathf.Infinity, minZ = Mathf.Infinity, maxZ = -Mathf.Infinity;
-            foreach(Vector3 vertex in vertices) {
-                if(vertex.x < minX) minX = vertex.x;
-                if(vertex.x > maxX) maxX = vertex.x;
-                if(vertex.z < minZ) minZ = vertex.z;
-                if(vertex.z > maxZ) maxZ = vertex.z;
             }
             Vector3 offset = new Vector3(minX + ((maxX - minX) / 2), 0, minZ + ((maxZ - minZ) / 2));
             for(j = 0; j < vertices.Length; j++)
@@ -61,17 +59,38 @@ public class Data : MonoBehaviour {
                 foreach(Vector3 vertex in vertices)
                     Debug.Log(vertex);
             }
+            float randHeight = lsystem.randHeight();
+            building.AddComponent("BoxCollider");
+            building.GetComponent<BoxCollider>().center = new Vector3(0, randHeight / 2, 0);
+            building.GetComponent<BoxCollider>().size = new Vector3(maxX - minX, randHeight, maxZ - minZ);
+
             if(height == 0)
-                lsystem.visualize(building/*this.gameObject*/, vertices, offset, lsystem.randHeight());
+                lsystem.visualize(building/*this.gameObject*/, vertices, offset, randHeight);
          //   else
          //      lsystem.visualize(building, vertices, height);
             if (i % 100 == 0 && i > 0)
                 Debug.Log("Buildings created: " + i);
         }
+
+        List<List<double[]>> roadData = new List<List<double[]>>();
+        roadData = parser.read("Highway.txt");
+        for (int i = 0; i < roadData.Count; i++) {
+            if (data[i].Count <= 2)
+                continue;
+            int j = 0;
+            foreach (double[] dArray in data[i]) {
+                if (j < data[i].Count - 1) { // Ignore last node, since it's equal to the first
+                    Vector3 pos = new Vector3((float)dArray[0], 0, (float)dArray[1]);
+                    if(PathFinder.nodeMap.ContainsKey(pos))
+                //        PathFinder.nodeMap[pos].addNeighbours(
+                    j++;
+                }
+            }
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
 	
 	}
 }
---
+++ PCGProject4.0/Assets/PathFinder.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathFinder : MonoBehaviour {
+    ArrayList nodes = new ArrayList();
+    Ray ray = new Ray();
+    RaycastHit hit;
+    Node startNode, endNode;
+    bool endSelect = false;
+    bool waitForAStar = false;
+    public static Dictionary<Vector3, Node> nodeMap = new Dictionary<Vector3, Node>();
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void addNode(Vector3 pos) {
+        GameObject nodeObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+     //   Node node = new Node(nodeObject);
+    //    nodes.Add(node);
+    //    node.setPosition(pos);
+    }
+
+    void selectNode(bool first) {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!waitForAStar) {
+            if (Input.GetMouseButtonUp(0)) {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
+            if (Physics.Raycast(ray, out hit)) {
+                if (!endSelect) {
+               //     startNode = new Node(hit.collider.gameObject);
+                //    hit.po
+                //    endSelect = true;
+                }
+                else {
+                 //   endNode = new Node(hit.collider.gameObject);
+                    FindPath();
+                    waitForAStar = true;
+                }
+            }
+        }
+	}
+
+    ArrayList FindPath() {
+        ArrayList path = new ArrayList();
+
+        ArrayList open = new ArrayList();
+        ArrayList closed = new ArrayList();
+
+        ArrayList successors = new ArrayList();
+
+        open.Add(startNode);
+
+        while (open.Count > 0) {
+            Node current = (Node)open[open.Count-1];
+            open.RemoveAt(open.Count - 1);
+            if(current == endNode){
+                while (current != null) {
+                    path.Add(current);
+                    current = current.getParent();
+                }
+                break;
+            }
+            current.getSuccessors(successors);
+            foreach (Node successor in successors) {
+                Node isOpen = null;
+                if (open.Contains(successor))
+                    isOpen = (Node)open[open.IndexOf(successor)];
+                if (isOpen != null && successor.getTotalCost() > isOpen.getTotalCost())
+                    continue;
+                Node isClosed = null;
+                if (closed.Contains(successor))
+                    isClosed = (Node)closed[closed.IndexOf(successor)];
+                if (isClosed != null && successor.getTotalCost() > isClosed.getTotalCost())
+                    continue;
+
+                open.Remove(isOpen);
+                closed.Remove(isClosed);
+                open.Add(successor);
+            }
+            closed.Add(current);
+        }
+
+        waitForAStar = false;
+        return path;
+    }
+}
