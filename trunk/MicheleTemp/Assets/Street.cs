44cab24f46d150bac8e1dc578734a387927f72bf
ermac988@gmail.com <ermac988@gmail.com@af11f6f2-b44f-d57f-2de4-35ac3e98ffae>
2012-11-27 18:57:08 +0000

    raods can now have smooth bends and they "try" (is not perfect) the heightmap

--- trunk/MicheleTemp/Assets/Street.cs
+++ trunk/MicheleTemp/Assets/Street.cs
@@ -19,10 +19,22 @@ public class Street : MonoBehaviour {
         data = parser.read("Highway.txt");
         //AttachedPathScript path = GetComponent <AttachedPathScript>();
 
-      
 
+        //bool enter = false;
        Terrain terComponent = (Terrain)gameObject.GetComponent(typeof(Terrain));
-     
+       TerrainPathCell[] terrainCells = new TerrainPathCell[terComponent.terrainData.heightmapResolution * terComponent.terrainData.heightmapResolution]; ;
+       float[,] terrainHeights = terComponent.terrainData.GetHeights(0, 0, terComponent.terrainData.heightmapResolution, terComponent.terrainData.heightmapResolution);
+       for (int x = 0; x < terComponent.terrainData.heightmapResolution; x++)
+       {
+           for (int y = 0; y < terComponent.terrainData.heightmapResolution; y++)
+           {
+               terrainCells[(y) + (x * terComponent.terrainData.heightmapResolution)].position.y = y;
+               terrainCells[(y) + (x * terComponent.terrainData.heightmapResolution)].position.x = x;
+               terrainCells[(y) + (x * terComponent.terrainData.heightmapResolution)].heightAtCell = terrainHeights[y, x];
+               terrainCells[(y) + (x * terComponent.terrainData.heightmapResolution)].isAdded = false;
+           }
+       }
+
        for(int i=0; i<10;i++)//data.Count; i++)
        {
            List<double[]> temp = data[i];
@@ -40,12 +52,13 @@ public class Street : MonoBehaviour {
            APS.NewPath();
            APS.pathTexture = 1;
            APS.isRoad = true;
+           APS.pathSmooth = 60;
            //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
            //cube.AddComponent<Rigidbody>();
            //cube.transform.position = new Vector3(, y, 0);ù
            for(int j=0; j<temp.Count;j++)
            {
-               Vector3 temp1;
+               //Vector3 temp1;
                //the y is taken calculating the height of the terrain in t hat point
                //Debug.Log(Terrain.activeTerrain.terrainData.detailWidth + " " +Terrain.activeTerrain.terrainData.size.x);
                //temp1 = new Vector3((float)temp[j][0] * 10000000 , Terrain.activeTerrain.SampleHeight(new Vector3((float)temp[j][0], (float)temp[j][1])), (float)temp[j][1]);
@@ -59,19 +72,16 @@ public class Street : MonoBehaviour {
                TerrainPathCell pathNodeCell = new TerrainPathCell();
                pathNodeCell.position.x = Mathf.RoundToInt((float)((((temp[j][0] * 1000000000000000) % Terrain.activeTerrain.terrainData.size.x) / Terrain.activeTerrain.terrainData.size.x) * Terrain.activeTerrain.terrainData.heightmapResolution));
                pathNodeCell.position.y = Mathf.RoundToInt((float)((((temp[j][1] * 1000000000000000) % Terrain.activeTerrain.terrainData.size.z) / Terrain.activeTerrain.terrainData.size.z) * Terrain.activeTerrain.terrainData.heightmapResolution)); ;
-               pathNodeCell.heightAtCell = Terrain.activeTerrain.SampleHeight(new Vector3((float)temp[j][0], (float)temp[j][1]));
-               Debug.Log("path node " + pathNodeCell.position);
+               pathNodeCell.heightAtCell = Terrain.activeTerrain.SampleHeight(new Vector3(pathNodeCell.position.x, pathNodeCell.position.y));
+               //Debug.Log("path node " + pathNodeCell.position);
                APS.CreatePathNode(pathNodeCell);
-               APS.addNodeMode = false;
+              
 
                
            }
+           APS.addNodeMode = false;
            //Debug.Log(APS.nodeObjects.Length);
-          
-
-           // define terrain cells
-           APS.terrainCells = new TerrainPathCell[APS.terData.heightmapResolution * APS.terData.heightmapResolution];
-
+           /*
            for (int x = 0; x < APS.terData.heightmapResolution; x++)
            {
                for (int y = 0; y < APS.terData.heightmapResolution; y++)
@@ -82,6 +92,12 @@ public class Street : MonoBehaviour {
                    APS.terrainCells[(y) + (x * APS.terData.heightmapResolution)].isAdded = false;
                }
            }
+            * */
+           // define terrain cells
+           APS.terrainCells = new TerrainPathCell[APS.terData.heightmapResolution * APS.terData.heightmapResolution];
+
+           //prova
+           APS.terrainCells = terrainCells;
          
            //APS.CreatePath(APS.pathSmooth, true, false);
            APS.pathMesh.renderer.enabled = true;
