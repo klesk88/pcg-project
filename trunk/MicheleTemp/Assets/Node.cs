21a932b62de76c91c97bbfae4d5afbefe565961d
vAcuMo@gmail.com <vAcuMo@gmail.com@af11f6f2-b44f-d57f-2de4-35ac3e98ffae>
2012-11-28 23:50:01 +0000

    Ah forgot these as well. They're not being used yet though

---
+++ trunk/MicheleTemp/Assets/Node.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class Node {
+    GameObject gameObject;
+    ArrayList neighbours;
+    Vector3 position;
+
+    public Node(Vector3 _position, Vector3[] _neighbours, GameObject _gameObject = null) {
+        position = _position;
+        if (_gameObject != null) {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Object.Destroy(cube.GetComponent<BoxCollider>());
+            cube.transform.position = position;
+            cube.transform.localScale = new Vector3(1.5f, 0.2f, 0.6f);
+            cube.transform.parent = _gameObject.transform;
+            float zxRelation = (_neighbours.Length == 2 ? (_neighbours[0].z - _neighbours[1].z) / ((_neighbours[0].x - _neighbours[1].x != 0 ? _neighbours[0].x - _neighbours[1].x : 1)) : (_position.z - _neighbours[0].z) / ((_position.x - _neighbours[0].x != 0 ? _position.x - _neighbours[0].x : 1)));
+            cube.transform.Rotate(new Vector3(0, -Mathf.Rad2Deg * Mathf.Atan(zxRelation), 0));
+            gameObject = cube;
+        }
+        else
+            gameObject = _gameObject;
+        
+        neighbours = new ArrayList(); 
+        foreach (Vector3 pos in _neighbours)
+            neighbours.Add(pos);
+    }
+
+    public void addNeighbours(Vector3[] newNeighbours) {
+        foreach (Vector3 neighbour in newNeighbours)
+            neighbours.Add(neighbour);
+    }
+
+    public GameObject getGameObject() { return gameObject; }
+    public Vector3 getPosition() { return position; }
+
+    public void getNeighbours(ArrayList _neighbours) {
+        _neighbours.Clear();
+        for (int i = 0; i < neighbours.Count; i++)
+            _neighbours.Add(PathFinder.nodeMap[(Vector3)neighbours[i]]);
+    }
+}
---
+++ trunk/MicheleTemp/Assets/PathFinder.cs
@@ -0,0 +1,128 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathFinder : MonoBehaviour {
+    Ray ray = new Ray();
+    RaycastHit hit;
+    Node startNode, endNode;
+    bool endSelect = false;
+    bool waitForAStar = false;
+
+    Dictionary<Node, Node> cameFrom = new Dictionary<Node, Node>();
+    Dictionary<Node, float> gScore = new Dictionary<Node, float>();
+    Dictionary<Node, float> fScore = new Dictionary<Node, float>();
+
+    ArrayList path = new ArrayList();
+    ArrayList open = new ArrayList();
+    ArrayList closed = new ArrayList();
+    ArrayList neighbours = new ArrayList();
+
+    public static Dictionary<Vector3, Node> nodeMap = new Dictionary<Vector3, Node>();
+ 	
+	// Update is called once per frame
+	void Update () {
+        if (!waitForAStar) {
+            if (Input.GetMouseButtonUp(0)) {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit)) {
+                    if (!endSelect) {
+                        startNode = nearestNode(hit.point);
+                        startNode.getGameObject().renderer.material.color = Color.green;
+                        Debug.Log("START NODE COORDINATES: " + startNode.getPosition());
+                        endSelect = true;
+                    }
+                    else {
+                        endNode = nearestNode(hit.point);
+                        endNode.getGameObject().renderer.material.color = Color.green;
+                        Debug.Log("GOAL NODE COORDINATES: " + endNode.getPosition());
+                        waitForAStar = true;
+                        ArrayList bestPath = FindPath();
+                        endSelect = false;
+                    }
+                }
+            }
+        }
+	}
+
+    ArrayList FindPath() {
+        cameFrom.Clear();
+        gScore.Clear();
+        fScore.Clear();
+        path.Clear();
+        open.Clear();
+        closed.Clear();
+        neighbours.Clear();
+
+        float tentativeGScore = 0;
+        gScore[startNode] = 0;
+        fScore[startNode] = gScore[startNode] + Vector3.Distance(startNode.getPosition(), endNode.getPosition());
+        open.Add(startNode);
+
+        int i = 0;
+        while (open.Count > 0 && i < 10000) {
+            Node current = lowestFScore();
+            if(current == endNode){
+                while (current != null) {
+                    path.Add(current);
+                    current = cameFrom.ContainsKey(current) ? cameFrom[current] : null;
+                }
+                break;
+            }
+            open.RemoveAt(open.IndexOf(current));
+            closed.Add(current);
+            current.getNeighbours(neighbours);
+            foreach (Node neighbour in neighbours) {
+                if (closed.Contains(neighbour))
+                    continue;
+
+                tentativeGScore = gScore[current] + Vector3.Distance(current.getPosition(), neighbour.getPosition());
+
+                if (!open.Contains(neighbour) || tentativeGScore <= gScore[neighbour]) {
+                    cameFrom[neighbour] = current;
+                    gScore[neighbour] = tentativeGScore;
+                    fScore[neighbour] = gScore[neighbour] + Vector3.Distance(neighbour.getPosition(), endNode.getPosition());
+                    if (!open.Contains(neighbour))
+                        open.Add(neighbour);
+                }
+            }
+            i++;
+        }
+        if (path.Count > 0)
+            Debug.Log("Path succesfully found!" + "Path Distance: " + tentativeGScore*15 + " m (estimate)");
+        else {
+            Debug.Log("It was not possible to find a path.");
+            startNode.getGameObject().renderer.material.color = Color.grey;
+            endNode.getGameObject().renderer.material.color = Color.grey;
+        }
+        foreach (Node node in path)
+            node.getGameObject().renderer.material.color = Color.red;
+        waitForAStar = false;
+        return path;
+    }
+
+    Node lowestFScore() {
+        Node lowestNode = new Node(Vector3.zero, new Vector3[1]);
+        float lowestfScore = Mathf.Infinity;
+        foreach (Node node in open) {
+            if (fScore[node] < lowestfScore) {
+                lowestfScore = fScore[node];
+                lowestNode = node;
+            }
+        }
+        return lowestNode;
+    }
+
+    public Node nearestNode(Vector3 pos) {
+        float closestDist = Mathf.Infinity;
+        Vector3 closestNode = Vector3.zero;
+        foreach (Vector3 nodePos in nodeMap.Keys) {
+            float dist = Vector3.Distance(pos, nodePos);
+            if (dist < closestDist) {
+                closestDist = dist;
+                closestNode = nodePos;
+            }
+        }
+        return (Node)nodeMap[closestNode];
+    }
+}
