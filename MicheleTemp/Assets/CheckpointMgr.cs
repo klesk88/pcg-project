a98317c55ddd248cab4ab2022e0c8b702ce2b5d8
vAcuMo@gmail.com <vAcuMo@gmail.com@af11f6f2-b44f-d57f-2de4-35ac3e98ffae>
2012-12-06 19:45:28 +0000

    - Prefab of car and checkpoint instead - Player can now load from latest checkpoint by pressing T - Car and checkpoint orientation needs to be calculated when they spawn, depending on the road direction (Should just be orientation of vector between the two path nodes to each side of the spawn point)

---
+++ MicheleTemp/Assets/CheckpointMgr.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class CheckpointMgr : MonoBehaviour {
+
+    public static Vector3 latestCheckpoint = Vector3.zero;
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.name == "Collider_Bottom") {
+            Debug.Log("Checkpoint passed! Press T to start from this location");
+            latestCheckpoint = transform.position + new Vector3(10, 0, 0);
+        }
+    }
+}
